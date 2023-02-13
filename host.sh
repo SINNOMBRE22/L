4d86053d326f7bcde56b1a2fe1b84ea05a2c4b4a
termuxbyte <50546720+termuxbyte@users.noreply.github.com>
2023-02-13 08:15:37 -0500

    Update host.sh

--- host.sh
+++ host.sh
@@ -139,28 +139,8 @@ restartprogram() {
 }
 menu() {
 echo "  "
-echo $white"\a     NO!"$red"                          MNO!   "
-echo $white"     MNO!!"$red"         [AMR]"$red"          MNNOO!    "
-echo $white"   MMNO!"$red"                           MNNOO!! "
-echo $white"  MNOONNOO!"$red"   MMMMMMMMMM"$white"PPPOII!"$red"   MNNO!!!!  "
-echo $white" !O! NNO!"$red" MMMMMMMMMMMMM"$white"PPPOOOII!!"$red" NO!       "
-echo $red"       ! MMMMMMMMMMMMM"$white"PPPPOOOOIII! !       "
-echo $red"        MMMMMMMMMMMM"$white"PPPPPOOOOOOII!!       "
-echo $red"        MMMMMOOOOOO"$white"PPPPPPPPOOOOMII!       "
-echo $red"        MMMMM..    OPP"$white"MMP    .,OMI!       "
-echo $red"        MMMM::"$purple"   o.,"$red"OP"$white"MP"$purple",.o"$white"   ::I!!       "
-echo $red"          NNM:::.,,OOPM!P"$white",.::::!!         "
-echo $red"         MMNNNNNOOOOPMO"$white"!!IIPPO!!O!        "
-echo $red"         MMMMMNNNNOO"$white":!!:!!IPPPPOO!        "
-echo $red"          MMMMMNNOOMMNN"$white"IIIPPPOO!!         "
-echo $red"             MMMONNMMNNN"$white"IIIOO!                "
-echo $white"           MN"$red" MOMMMNNN"$white"IIIIIO!"$red" OO          "
-echo $white"          MNO! "$red"iiiiii"$white"iiiiiI"$red" OOOO         "
-echo $white"     NNN.MNO!   "$red"O"$yellow"!!!!!!!!!"$white"O"$red"   OONO NO!    "
-echo $white"    MNNNNNO!    "$red"OOOOO"$white"OOOOOO"$red"    MMNNON!    "
-echo $white"      MNNNNO!    "$red"PPP"$white"PPPPPP"$red"    MMNON!      "
-echo $white"         OO!"$red"                   ON!        "
-echo $white
+echo "Buscar host disponibles"
+
 echo '''
 	~{1}--DNS Lookup
 	~{2}--Reverse DNS Lookup
