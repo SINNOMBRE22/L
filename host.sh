07f1b685d27e31ad8c1ed58f1eaf384a592cf991
termuxbyte <50546720+termuxbyte@users.noreply.github.com>
2023-02-13 08:13:38 -0500

    Update host.sh

--- host.sh
+++ host.sh
@@ -21,7 +21,46 @@ else
 	exit
 fi
 }
-
+whois() {
+	echo "\aInput Domain [ex : example.com]"
+                echo $white "╭─"$green"AMR@localhost"$cyan" ~/INmux "$white
+                read -p " ╰─>~#  "  target
+                        command=$(wget http://api.hackertarget.com/whois/?q=$target -q -O .-)
+			echo $yellow"\a["$red"+"$yellow"]"$white Whois Lookup
+			echo $green
+                                cat .-
+				reas
+}
+dnslook() {
+	echo "\aInput Domain [ex : example.com]"
+		echo $white "╭─"$green"AMR@localhost"$cyan" ~/INmux "$white
+		read -p " ╰─>~#  "  target
+			command=$(wget http://api.hackertarget.com/dnslookup/?q=$target -q -O .-)
+			echo $yellow"\a["$red"+"$yellow"]"$white DNS Lookup
+			echo $green
+				cat .-
+				reas
+}
+revdnslook() {
+	echo "\aInput Domain [ex : example.com]"
+		echo $white "╭─"$green"AMR@localhost"$cyan" ~/INmux "$white
+		read -p " ╰─>~#  "  target
+			command=$(wget http://api.hackertarget.com/reverseiplookup/?q=$target -q -O .-)
+			echo $yellow"\a["$red"+"$yellow"]"$white Reverse DNS Lookup
+			echo $green
+				cat .-
+				reas
+}
+geoip() {
+	echo "\aInput IP Address [ex : 123.456.789] or [ex : example.com]"
+		echo $white "╭─"$green"AMR@localhost"$cyan" ~/INmux "$white
+		read -p " ╰─>~#  "  target
+			command=$(wget http://api.hackertarget.com/geoip/?q=$target -q -O .-)
+			echo $yellow"\a["$red"+"$yellow"]"$white Geolocation Ip
+			echo $green
+				cat .-
+				reas
+}
 hostfinder() {
 	echo "\aInput Domain [ex : example.com]"
 		echo $white "╭─"$green"AMR@localhost"$cyan" ~/INmux "$white
@@ -31,7 +70,67 @@ hostfinder() {
 			echo $green
 				cat .-
 				reas
-
+}
+httpheader() {
+	echo "\aInput Domain [ex : example.com]"
+		echo $white "╭─"$green"AMR@localhost"$cyan" ~/INmux "$white
+		read -p " ╰─>~#  "  target
+			command=$(wget http://api.hackertarget.com/httpheaders/?q=$target -q -O .-)
+			echo $yellow"\a["$red"+"$yellow"]"$white Http Header
+			echo $green
+				cat .-
+				reas
+}
+hostdns() {
+	echo "\aInput Domain [ex : example.com]"
+		echo $white "╭─"$green"AMR@localhost"$cyan" ~/INmux "$white
+		read -p " ╰─>~#  "  target
+			command=$(wget https://api.hackertarget.com/mtr/?q=$target -q -O .-)
+			echo $yellow"\a["$red"+"$yellow"]"$white Host DNS
+			echo $green
+				cat .-
+				reas
+}
+portscan() {
+	echo "\aInput Domain [ex : example.com]"
+		echo $white "╭─"$green"AMR@localhost"$cyan" ~/INmux "$white
+		read -p " ╰─>~#  "  target
+			command=$(wget https://api.hackertarget.com/mtr/?q=$target -q -O .-)
+			echo $yellow"\a["$red"+"$yellow"]"$white Portscan
+			echo $green
+				cat .-
+				reas
+}
+subnet() {
+	echo "\aInput Ip Address [ex : 123.456.789] "
+		echo $white "╭─"$green"AMR@localhost"$cyan" ~/INmux "$white
+		read -p " ╰─>~#  "  target
+			command=$(wget https://api.hackertarget.com/subnetcalc/?q=$target -q -O .-)
+			echo $yellow"\a["$red"+"$yellow"]"$white Subnet
+			echo $green
+				cat .-
+				reas
+}
+zonetransfer() {
+	echo "\aInput Domain [ex : example.com]"
+		echo $white "╭─"$green"AMR@localhost"$cyan" ~/INmux "$white
+		read -p " ╰─>~#  "  target
+			command=$(wget https://api.hackertarget.com/zonetransfer/?q=$target -q -O .-)
+			echo $yellow"\a["$red"+"$yellow"]"$white ZONE Transfer
+			echo $green
+				cat .-
+				reas
+}
+extraclink() {
+	echo "\aInput Domain [ex : example.com]"
+		echo $white "╭─"$green"AMR@localhost"$cyan" ~/INmux "$white
+		read -p " ╰─>~#  "  target
+			command=$(wget https://api.hackertarget.com/pagelinks/?q=$target -q -O .-)
+			echo $yellow"\a["$red"+"$yellow"]"$white Extraclink
+			echo $green
+				cat .-
+				reas
+}
 restartprogram() {
 	sleep 1
 	echo "\a Permission Denied"
@@ -39,10 +138,43 @@ restartprogram() {
 	menu
 }
 menu() {
+echo "  "
+echo $white"\a     NO!"$red"                          MNO!   "
+echo $white"     MNO!!"$red"         [AMR]"$red"          MNNOO!    "
+echo $white"   MMNO!"$red"                           MNNOO!! "
+echo $white"  MNOONNOO!"$red"   MMMMMMMMMM"$white"PPPOII!"$red"   MNNO!!!!  "
+echo $white" !O! NNO!"$red" MMMMMMMMMMMMM"$white"PPPOOOII!!"$red" NO!       "
+echo $red"       ! MMMMMMMMMMMMM"$white"PPPPOOOOIII! !       "
+echo $red"        MMMMMMMMMMMM"$white"PPPPPOOOOOOII!!       "
+echo $red"        MMMMMOOOOOO"$white"PPPPPPPPOOOOMII!       "
+echo $red"        MMMMM..    OPP"$white"MMP    .,OMI!       "
+echo $red"        MMMM::"$purple"   o.,"$red"OP"$white"MP"$purple",.o"$white"   ::I!!       "
+echo $red"          NNM:::.,,OOPM!P"$white",.::::!!         "
+echo $red"         MMNNNNNOOOOPMO"$white"!!IIPPO!!O!        "
+echo $red"         MMMMMNNNNOO"$white":!!:!!IPPPPOO!        "
+echo $red"          MMMMMNNOOMMNN"$white"IIIPPPOO!!         "
+echo $red"             MMMONNMMNNN"$white"IIIOO!                "
+echo $white"           MN"$red" MOMMMNNN"$white"IIIIIO!"$red" OO          "
+echo $white"          MNO! "$red"iiiiii"$white"iiiiiI"$red" OOOO         "
+echo $white"     NNN.MNO!   "$red"O"$yellow"!!!!!!!!!"$white"O"$red"   OONO NO!    "
+echo $white"    MNNNNNO!    "$red"OOOOO"$white"OOOOOO"$red"    MMNNON!    "
+echo $white"      MNNNNO!    "$red"PPP"$white"PPPPPP"$red"    MMNON!      "
+echo $white"         OO!"$red"                   ON!        "
+echo $white
 echo '''
-
-	~{1}--Buscar Host
-	~{99}-Salir
+	~{1}--DNS Lookup
+	~{2}--Reverse DNS Lookup
+	~{3}--Whois Lookup
+	~{4}--GeoIP Lookup
+	~{5}--Host Finder
+	~{6}--HTTP Header
+	~{7}--Host DNS Finder
+	~{8}--Port Scanner
+	~{9}--Subnet Lookup
+	~{10}-Zone Transfer
+	~{11}-Exatrac Links
+	~{00}-Quit
+	~{99}-About
 '''
 
 echo $white "╭─"$green"AMR@localhost"$cyan" ~/INmux"$white
